0e29f4113714bd9fb53a85563eb938bee3abc9e9
leNam199320xx <lenam19932012@gmail.com>
2023-12-22 20:28:10 +0700

    Create luckydraw.js

---
+++ luckydraw.js
@@ -0,0 +1,29 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+var degree = 360;
+function preload ()
+{
+}
+
+function create ()
+{
+}
+
+function update ()
+{
+}
+
+function drawCircle(listOfItem){
+
+  
+}
