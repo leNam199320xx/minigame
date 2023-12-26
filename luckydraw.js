780412a493e5fd755006225569288510da8a6900
leNam199320xx <lenam19932012@gmail.com>
2023-12-26 08:46:00 +0700

    update

--- luckydraw.js
+++ luckydraw.js
@@ -1,3 +1,5 @@
+import "./phaser.js";
+
 var config = {
     type: Phaser.AUTO,
     width: 800,
@@ -12,43 +14,61 @@ var config = {
 var game = new Phaser.Game(config);
 var graphics;
 var degree = 360;
-function preload ()
-{
+var isStart = false;
+var count = 1;
+
+function preload() {
+    game.load.image('button', 'btn-start-80.png');
 }
 
-function create ()
-{
+function create() {
+    game.stage.backgroundColor = "#4488AA";
     graphics = this.add.graphics();
+
+    button = game.add.button(0, 80, 'button', actionOnClick, this, 'over', 'out', 'down');
+
 }
 
-function update ()
-{        
+function over() {
+    console.log('button over');
+}
+
+function out() {
+    console.log('button out');
+}
+
+function actionOnClick() {
+    isStart = true;
+}
+
+function update() {
     graphics.clear();
-    drawCircle(["test1", "test2"]);
+    if (isStart) {
+        drawCircle(["test1", "test2"]);
+        drawClockWise();
+    }
+    else {
+        button.visible = false;
+    }
 }
 
-function drawCircle(listOfItem){
-    if(typeof(listOfItem) == "object"){
-        graphics.lineStyle(4, 0xff00ff, 1);
-
+function drawCircle(listOfItem) {
+    if (typeof (listOfItem) == "object") {
+        count = listOfItem.length;
+        var startCorner = 0;
         //  Without this the arc will appear closed when stroked
-        graphics.beginPath();
-
-        // arc (x, y, radius, startAngle, endAngle, anticlockwise)
-        graphics.slice(400, 300, 100, Phaser.Math.DegToRad(90), Phaser.Math.DegToRad(180), true);
-
-        graphics.strokePath();
-
-        //  Without this the arc will appear closed when stroked
-        graphics.beginPath();
-
-        graphics.lineStyle(4, 0x0000ff, 1);
-        // arc (x, y, radius, startAngle, endAngle, anticlockwise)
-        graphics.slice(400, 300, 100, Phaser.Math.DegToRad(180), Phaser.Math.DegToRad(90), true);
-        //  Uncomment this to close the path before stroking
-        // graphics.closePath();
-
-        graphics.strokePath();
+        for(var i =0; i < count; i++){
+            var corner = 360 / count;
+            graphics.lineStyle(4, 0xff00ff, 1);
+            graphics.beginPath();
+            graphics.slice(400, 300, 100, Phaser.Math.DegToRad(startCorner), Phaser.Math.DegToRad(corner), true);
+            graphics.strokePath();
+            startCorner += corner;
+        }
     }
-  
+
+}
+
+function drawClockWise() {
+
 }
